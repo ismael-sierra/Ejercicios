cd6b29e507f9c6ed63dc57f4929745c1d078a079
ismael-sierra <ismaelsierravega2003@gmail.com>
2025-03-12 17:07:17 +0100

    script.js retocado

--- 21. Ejercicio API's - creación de un buscador de pokémon/script.js
+++ 21. Ejercicio API's - creación de un buscador de pokémon/script.js
@@ -1,32 +1,40 @@
 document
   .getElementById("pokemon-form")
   .addEventListener("submit", async function (event) {
+    // Con async/await manejamos errores sin necesidad de lanzar excepciones manualmente con throw,
+    //  además de mejorar la legibilidad y facilitar la depuración del código
     event.preventDefault();
+
     const pokemonName = document
       .getElementById("pokemon-name")
       .value.toLowerCase()
-      .trim();
+      .trim(); // Eliminamos espacios en blanco al inicio y final
+
     const errorMessage = document.getElementById("error-message");
     const table = document.getElementById("pokemon-table");
     const tbody = document.getElementById("pokemon-data");
+
     errorMessage.classList.add("oculto");
     table.classList.add("oculto");
-    tbody.innerHTML = "";
+    tbody.innerHTML = ""; // Borramos el contenido de la tabla para limpiar datos anteriores
 
     if (!pokemonName) {
       errorMessage.textContent = "Por favor, introduce un nombre de Pokémon.";
-      errorMessage.classList.remove("oculto");
+      errorMessage.classList.remove("oculto"); // Enseñamos el mensaje de error
       return;
     }
 
     try {
       const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-      );
+      ); // Hacemos la solicitud a la PokeAPI con el nombre del Pokemon
       if (!response.ok) {
         throw new Error("Pokémon no encontrado.");
       }
-      const data = await response.json();
+
+      const data = await response.json(); // Convertimos la respuesta en un objeto JSON
+
+      // Extraemos las habilidades y tipos del Pokémon
       const habilidades = data.abilities.map((a) => a.ability.name).join(", ");
       const tipos = data.types.map((t) => t.type.name).join(", ");
 
@@ -41,6 +49,7 @@ document
       tbody.appendChild(row);
       table.classList.remove("oculto");
     } catch (error) {
+      // Si ocurriera un error durante la solicitud mostramos y enseñamos el mensaje de error
       errorMessage.textContent = error.message;
       errorMessage.classList.remove("oculto");
     }
